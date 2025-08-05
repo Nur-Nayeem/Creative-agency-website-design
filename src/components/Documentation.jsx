1adb497cc66617e2e460524757abb8a36506ff4e
nur-nayeem <nurnayem768@gmail.com>
2025-08-06 02:52:16 +0600

    added documentation section

---
+++ src/components/Documentation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../css/documentation.css'
+import videoImg from '../assets/video.png'
+function Documentation() {
+    return (
+        <div>
+            <div className='documentation-container'>
+                <h4 className='section-title'>Our Services</h4>
+                <h3 className='sub-title'>The various services we provide to make your business more powerful</h3>
+                <figure className='video-box'>
+                    <img src={videoImg} alt="" />
+                </figure>
+
+            </div>
+        </div>
+    )
+}
+
+export default Documentation
