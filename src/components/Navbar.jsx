5cd9540710b575939dc902e1309b5cf189e96884
nur-nayeem <nurnayem768@gmail.com>
2025-08-09 11:13:36 +0600

    added side nav menu for mobile device

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/navSection.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
+    const [showMenu, setShowMenu] = useState(false);
+    const handleManueBar = () => {
+        setShowMenu(true)
+    }
+    const handleCloseMenu = () => {
+        setShowMenu(false)
+    }
     return (
         <nav className='nav-section'>
             <div className='logo-container'>
                 <h2>C</h2>
             </div>
-            <ul className='nav-items'>
+            <ul className={`nav-items ${showMenu ? 'nav-items-active' : ''}`}>
+                <button className='menue-bar cross-icon-btn' onClick={handleCloseMenu}>
+                    <FontAwesomeIcon className='menu-cross-icon' icon={faXmark} />
+                </button>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#works">Works</a></li>
                 <li><a href="#about">About</a></li>
             </ul>
-            <button className='menue-bar'>
-                <FontAwesomeIcon className='menu' icon={faBars} />
+            <button className='menue-bar' onClick={handleManueBar}>
+                <FontAwesomeIcon className='menu' icon={faBars}
+                />
             </button>
             <button className='btn-primary contct-btn'>Contant us</button>
         </nav>
